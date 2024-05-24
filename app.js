55c67389528caea06e77b4b08118025116b96571
FIRSIKS <146068118+FIRSIKS@users.noreply.github.com>
2024-05-24 11:44:32 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+function writeResultToFile(filename, sum, product) {
+    fs.writeFile(filename, 'Сумма элементов массива A: ${sum}\nПроизведение элеметов масива A: ${product}', (err) => {
+        if(err) throw err;
+    console.log('Результат был записан в файл ${filename}');
+    });
+}
 let A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 let sum = 0;
 let product = 1;
@@ -8,4 +15,4 @@ for (let i = 0; i < A.length; i++) {
 }
 console.log("Сумма элементов массива A: " + sum);
 console.log("Произведение элементов массива A: " + product);
-writeResultToFile('result.txt');
+writeResultToFile('result.txt', sum, product);
